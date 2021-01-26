fc4e049f8c42bb14796e82ea2bd1035a976eb6d9
Richard Vu Tran <redsriracha@gmail.com>
2021-01-26 03:46:02 +0000

    Fixed comments on run.py

--- run.py
+++ run.py
@@ -2,14 +2,19 @@ import tensorflow as tf
 
 from util.models import MyModel
 
-# Output Tensorflow related build
+
 def print_tensorflow():
+    """
+    Output Tensorflow related build
+    """
     print("Tensorflow", tf.__version__)
     print("isBuild with CUDA:", tf.test.is_built_with_cuda())
 
 
-# Output GPU(s) info that is compatible to CUDA on the machine
 def print_gpu_info():
+    """
+    Output GPU(s) info that is compatible to CUDA on the machine
+    """
     print("Number of GPU(s):", len(tf.config.list_physical_devices('GPU')))
     print("GPU(s):", tf.config.list_physical_devices('GPU'))
 
